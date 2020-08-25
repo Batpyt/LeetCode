ffccebd0a7050ea6e6619389b497b01f36886f18
Batpyt <Batpyt@users.noreply.github.com>
2020-08-26 01:29:00 +0800

    求二叉树深度
    递归返回最大深度的子节点

---
+++ 树/Shendu.java
@@ -0,0 +1,17 @@
+package 树;
+/*
+输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+/*
+通过dfs，每一层都返回左子树和右子树中更深的那一个。
+ */
+public class Shendu {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+
+}
