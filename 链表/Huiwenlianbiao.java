f80abdd5697637a01c563f664516ce5211d098b4
tianyuping <tianyuping@fang.com>
2020-10-23 10:10:39 +0800

    回文链表

---
+++ 链表/Huiwenlianbiao.java
@@ -0,0 +1,36 @@
+package 链表;
+
+public class Huiwenlianbiao {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        if(head == null || head.next == null) {
+            return true;
+        }
+
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow = reverse(slow.next);
+        while(slow != null) {
+            if(head.val != slow.val) return false;
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if(head.next == null) return head;
+        ListNode newhead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newhead;
+    }
+}
