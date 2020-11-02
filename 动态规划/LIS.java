278423411e2199bf48086d54d85d63edfd75f54e
tianyuping <tianyuping@fang.com>
2020-11-02 15:23:40 +0800

    最大上升子序列

---
+++ 动态规划/LIS.java
@@ -0,0 +1,49 @@
+package 动态规划;
+/*
+给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+示例:
+输入: [10,9,2,5,3,7,101,18]
+输出: 4
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/*
+动态规划，维护一个dp数组，里面存放的是子序列尾部元素的值，例如：dp[k]存放的是长度为 k+1长度子序列的尾部元素
+最终这个dp数组的长度为最大子序列的长度。dp呈递增。
+从头开始遍历，在dp中寻找当前元素的位置，若该元素大于dp最后一个值，dp[k+1] = 当前元素。
+ */
+
+public class LIS {
+
+    public static void main(String[] args) {
+        int[] nums = {10,9,2,5,3,7,101,18};
+        int[] n = {2,3,7,21};
+        System.out.println(lengthOfLIS(nums));
+    }
+    public static int lengthOfLIS(int[] nums) {
+        //dp数组
+        int[] dp = new int[nums.length];
+        //一开始的长度为0
+        int res = 0;
+        for(int num : nums) {
+            //用二分法查找元素应该所在的位置
+            int i = 0, j = res;
+            while(i < j) {
+                int m = (i + j) / 2;
+                if(dp[m] < num) i = m + 1;
+                else j = m;
+            }
+            //System.out.println(i + " " + num);
+            //将元素计入位置
+            dp[i] = num;
+            //当j==res时，说明当前元素加入到了k+1，长度也要+1
+            if(res == j) res++;
+        }
+        return res;
+    }
+
+}
