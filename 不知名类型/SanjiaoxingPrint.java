740f03779aa7332e3e766aee3b5412ce55ffccc9
tianyuping <tianyuping@fang.com>
2020-11-05 09:49:46 +0800

    三角形打印

---
+++ 不知名类型/SanjiaoxingPrint.java
@@ -0,0 +1,17 @@
+package 不知名类型;
+
+public class SanjiaoxingPrint {
+    public static void main(String[] args) {
+        int num = 9;
+        int k = 0;
+        for(int i = 0; i <= num; i++) {
+            for(int j = 0; j <= k; j++) {
+                System.out.print(i);
+
+
+            }
+            System.out.println();
+            k++;
+        }
+    }
+}
