f5d122fc0c1b6e3e0e983b9e7cd0ca708e7a0199
Batpyt <Batpyt@users.noreply.github.com>
2020-11-22 14:34:48 +0800

    合并时间序列/合并序列

---
+++ 动态规划/TimeMerge.java
@@ -0,0 +1,65 @@
+package 动态规划;
+
+import java.util.*;
+
+/**
+ * 合并数组
+ * [1,3][2,4][5,6] -> [1,6]
+ */
+
+public class TimeMerge {
+    public static void main(String[] args) {
+        int[][] time = {{2,4},{1,3},{5,6}};
+        int[][] res = timeMerge(time);
+        for(int[] r : res) {
+            for(int i : r) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] timeMerge(int[][] time) {
+        List<int[]> res = new LinkedList<>();
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o2[1] - o1[1];
+                }
+                else {
+                    return o1[0] -o2[0];
+                }
+            }
+        });
+
+//        for(int[] r : time) {
+//            for(int i : r) {
+//                System.out.print(i+" ");
+//            }
+//            System.out.println();
+//        }
+
+        int left = time[0][0];
+        int right = time[0][1];
+        for(int i = 1; i < time.length; i++) {
+            //13 24 56
+            //[1,3][4,5]
+            if(right < time[i][0] - 1) {
+                int[] add = {left,right};
+                res.add(add);
+                left = time[i][0];
+                right = time[i][1];
+            }
+
+            //[1,3][2,4]
+            //[1,3][1,4]
+            if(left <= time[i][0]) {
+                right = Math.max(right, time[i][1]);
+            }
+        }
+        int[] add = {left,right};
+        res.add(add);
+        return res.toArray(new int[res.size()][2]);
+    }
+}
