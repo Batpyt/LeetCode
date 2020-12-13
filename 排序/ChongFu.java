75d0ec152b77cd021ab2cbbe47548dfe8b9b7e32
Batpyt <Batpyt@users.noreply.github.com>
2020-12-13 10:06:26 +0800

    是否存在重复元素

---
+++ 排序/ChongFu.java
@@ -0,0 +1,37 @@
+package 排序;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,1]
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class ChongFu {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    //排序，对比相邻的数是否相等
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) return true;
+        }
+
+        return false;
+    }
+}
