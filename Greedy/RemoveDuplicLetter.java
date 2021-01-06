d23b1a4f07887aceb92addc9b3d7181a90753284
tianyuping <tianyuping@fang.com>
2021-01-06 18:01:24 +0800

    去除重复字母

---
+++ Greedy/RemoveDuplicLetter.java
@@ -0,0 +1,43 @@
+package Greedy;
+
+import java.util.Stack;
+
+/**
+ * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+ *
+ * 注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters 相同
+ * 示例 1：
+ *
+ * 输入：s = "bcabc"
+ * 输出："abc"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicate-letters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class RemoveDuplicLetter {
+    public static void main(String[] args) {
+        String s = "bcabc";
+        System.out.println(removeDuplicateLetters(s));
+    }
+
+    public static String removeDuplicateLetters(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if(stack.contains(c)) continue;
+            //遇到一个新字符 如果比栈顶小 并且在新字符后面还有和栈顶一样的 就把栈顶的字符抛弃了
+            while (!stack.isEmpty() && stack.peek() > c && s.indexOf(stack.peek(), i) != -1) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
