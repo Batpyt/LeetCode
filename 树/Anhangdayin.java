655438fbedd951b6517a0870d088230d71f17d81
tianyuping <tianyuping@fang.com>
2021-01-22 15:23:05 +0800

    按行打印-倒序

--- 树/Anhangdayin.java
+++ 树/Anhangdayin.java
@@ -96,6 +96,27 @@ public class Anhangdayin {
 		return ans;
     }
 
+    //倒序
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<>();
+		LinkedList<List<Integer>> res = new LinkedList<>();
+		if(root != null) queue.add(root);
+
+		while(!queue.isEmpty()) {
+			List<Integer> temp = new LinkedList<>();
+			int size = queue.size();
+			for(int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				temp.add(node.val);
+				if(node.left != null) queue.add(node.left);
+				if(node.right != null) queue.add(node.right);
+			}
+			res.addFirst(temp);
+		}
+		return res;
+	}
+
+
 
 
 }
