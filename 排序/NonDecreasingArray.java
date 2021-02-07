962a8fc80d04fafdf9a6c069e97458f4210f9de9
tianyuping <tianyuping@fang.com>
2021-02-07 16:45:46 +0800

    非递减数组

---
+++ 排序/NonDecreasingArray.java
@@ -0,0 +1,64 @@
+package 排序;
+
+/**
+ *
+ 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+
+ 我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+
+
+
+ 示例 1:
+
+ 输入: nums = [4,2,3]
+ 输出: true
+ 解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+ 示例 2:
+
+ 输入: nums = [4,2,1]
+ 输出: false
+ 解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+ */
+
+public class NonDecreasingArray {
+	public static void main(String[] args) {
+		int[] nums = {4,2,3};
+		System.out.println(checkPossibility(nums));
+	}
+
+	/*
+		x
+			x	-> 		x	x
+	x				x
+
+
+		x				x	x
+	x			->	x
+			x
+	 */
+	public static boolean checkPossibility(int[] nums) {
+		boolean changed = false;
+
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i] < nums[i - 1]) {
+				if(!changed) {
+					changed = true;
+					if(i >= 2 && nums[i] < nums[i - 2]) {
+						nums[i] = nums[i - 1];
+					}
+					else {
+						nums[i - 1] = nums[i];
+					}
+					for(int n : nums) {
+						System.out.print(n + " ");
+					}
+				}
+				else {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
