b9261bdbc8d9eb0be80c0c6a633eced503995a73
tianyuping <tianyuping@fang.com>
2021-02-07 16:46:34 +0800

    走格子

--- DFSs/Zougezi.java
+++ DFSs/Zougezi.java
@@ -29,44 +29,75 @@ public class Zougezi {
 			System.out.println(movingCount(m, n, k));
 		}
 	}
-	
-	/*
-	 * DFS方法，深度优先遍历。从(0,0)开始往下方和右方遍历。若i，j超过m，n，当前点已遍历过，或该点位数相加大于k，则返回0。
-	 */
-	public static int movingCount(int m,int n ,int k ) {
+
+	public static int movingCount(int m, int n, int k) {
 		int count = 0;
-		boolean[][] b= new boolean[m][n]; //用来记录是否遍历过
-		
-		//System.out.println(b[0][0]);
-		return dfs(0, 0, m, n, k, b);  
+		boolean[][] visited = new boolean[m][n];
+		return dfs(0,0,m,n,k,visited);
 	}
-	
-	public static int dfs(int i, int j, int m, int n, int k, boolean[][] b) {
-		if(i >= m || j >= n || b[i][j] || Sum(i, j) > k) {
+
+	public static int dfs(int i, int j, int m, int n, int k, boolean[][] visited) {
+		if(i >= m || j >= n || visited[i][j] || sum(i,j) > k) {
 			return 0;
 		}
-		//System.out.println(i+" "+j);
-		b[i][j] = true; //标记为已遍历过。
-		int res = dfs(i + 1, j, m, n, k, b) + dfs(i, j + 1, m, n, k, b);
-		
+		visited[i][j] = true;
+
+		int res = dfs(i + 1, j, m, n, k, visited) + dfs(i, j + 1, m, n, k, visited);
 		return res + 1;
 	}
-	
-	public static int Sum(int i, int j) {
+
+	public static int sum(int i, int j) {
+		int sum = 0;
 		Integer I = i;
 		Integer J = j;
-		int sum = 0;
 		String[] si = I.toString().split("");
 		String[] sj = J.toString().split("");
-		for(int a = 0; a < si.length; a++) {
-			sum = sum + Integer.parseInt(si[a]);
+		for(String s : si) {
+			sum += Integer.parseInt(s);
 		}
-		for(int b = 0; b < sj.length; b++) {
-			sum = sum + Integer.parseInt(sj[b]);
+		for(String s : sj) {
+			sum += Integer.parseInt(s);
 		}
-		//System.out.println(sum);
-		
 		return sum;
 	}
+	
+//	/*
+//	 * DFS方法，深度优先遍历。从(0,0)开始往下方和右方遍历。若i，j超过m，n，当前点已遍历过，或该点位数相加大于k，则返回0。
+//	 */
+//	public static int movingCount(int m,int n ,int k ) {
+//		int count = 0;
+//		boolean[][] b= new boolean[m][n]; //用来记录是否遍历过
+//
+//		//System.out.println(b[0][0]);
+//		return dfs(0, 0, m, n, k, b);
+//	}
+//
+//	public static int dfs(int i, int j, int m, int n, int k, boolean[][] b) {
+//		if(i >= m || j >= n || b[i][j] || Sum(i, j) > k) {
+//			return 0;
+//		}
+//		//System.out.println(i+" "+j);
+//		b[i][j] = true; //标记为已遍历过。
+//		int res = dfs(i + 1, j, m, n, k, b) + dfs(i, j + 1, m, n, k, b);
+//
+//		return res + 1;
+//	}
+//
+//	public static int Sum(int i, int j) {
+//		Integer I = i;
+//		Integer J = j;
+//		int sum = 0;
+//		String[] si = I.toString().split("");
+//		String[] sj = J.toString().split("");
+//		for(int a = 0; a < si.length; a++) {
+//			sum = sum + Integer.parseInt(si[a]);
+//		}
+//		for(int b = 0; b < sj.length; b++) {
+//			sum = sum + Integer.parseInt(sj[b]);
+//		}
+//		//System.out.println(sum);
+//
+//		return sum;
+//	}
 
 }
