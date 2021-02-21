c6ed53725344476be812a46232f18bac45d71631
Batpyt <Batpyt@users.noreply.github.com>
2021-02-21 18:20:45 +0800

    键值对合并

---
+++ 牛客机试/CombineKV.java
@@ -0,0 +1,51 @@
+package 牛客机试;
+
+import java.util.*;
+
+/**
+ * 题目描述
+ * 数据表记录包含表索引和数值（int范围的正整数），请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照key值升序进行输出。
+ *
+ * 输入描述:
+ * 先输入键值对的个数
+ * 然后输入成对的index和value值，以空格隔开
+ *
+ * 输出描述:
+ * 输出合并后的键值对（多行）
+ *
+ * 示例1
+ * 输入
+ * 复制
+ * 4
+ * 0 1
+ * 0 2
+ * 1 2
+ * 3 4
+ * 输出
+ * 复制
+ * 0 3
+ * 1 2
+ * 3 4
+ */
+
+public class CombineKV {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        Map<Integer, Integer> map = new TreeMap<>();
+        for(int i = 0; i < size; i++) {
+            String[] ss = sc.nextLine().split(" ");
+            int k = Integer.parseInt(ss[0]);
+            int v = Integer.parseInt(ss[1]);
+            if(map.containsKey(k)) {
+                map.put(k, map.get(k) + v);
+            }
+            else {
+                map.put(k, v);
+            }
+        }
+        for(Integer key : map.keySet()) {
+            System.out.println(key + " "+ map.get(key));
+        }
+    }
+}
