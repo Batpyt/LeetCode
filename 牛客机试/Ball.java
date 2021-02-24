c86a37e1b5affc27284f7cc32263fb624268a001
tianyuping <tianyuping@fang.com>
2021-02-24 15:41:04 +0800

    球反弹

---
+++ 牛客机试/Ball.java
@@ -0,0 +1,14 @@
+package 牛客机试;
+
+import java.util.*;
+
+public class Ball {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			int n = sc.nextInt();
+			System.out.println(n * 2.875);
+			System.out.println(n * 0.03125);
+		}
+	}
+}
