79fc58573846d665709c245cccc7b045d0e0d7c3
tianyuping <tianyuping@fang.com>
2021-03-05 09:34:11 +0800

    用栈实现队列

--- 栈/ImplementQueueUsingStack.java
+++ 栈/ImplementQueueUsingStack.java
@@ -17,7 +17,7 @@ import java.util.Stack;
  *
  * 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
  * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
- *  
+ *  123
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
