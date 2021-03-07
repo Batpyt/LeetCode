dae01e7fb961b6aad6770eaea80a6c124bd7894d
Batpyt <Batpyt@users.noreply.github.com>
2021-03-07 19:26:16 +0800

    两链表相加

--- 链表/SumtwoList.java
+++ 链表/SumtwoList.java
@@ -54,4 +54,60 @@ public class SumtwoList {
         }
         return head;
     }
+
+    /**
+     *
+     给定两个用链表表示的整数，每个节点包含一个数位。
+
+     这些数位是反向存放的，也就是个位排在链表首部。
+
+     编写函数对这两个整数求和，并用链表形式返回结果。
+
+
+
+     示例：
+
+     输入：(7 -> 1 -> 6) + (5 -> 9 -> 2)，即617 + 295
+     输出：2 -> 1 -> 9，即912
+     进阶：思考一下，假设这些数位是正向存放的，又该如何解决呢?
+
+     示例：
+
+     输入：(6 -> 1 -> 7) + (2 -> 9 -> 5)，即617 + 295
+     输出：9 -> 1 -> 2，即912
+     * @param l1
+     * @param l2
+     * @return
+     */
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        //用来记录进位的变量
+        int carry = 0;
+        //dummy head
+        ListNode res = new ListNode(0);
+        //真正的结果起始位置
+        ListNode cur = res;
+
+        //当l1，l2都遍历完再跳出循环
+        while (l1 != null || l2 != null) {
+            //记录l1/l2的值
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            //当前两个数的和
+            int sum = n1 + n2 + carry;
+            //记录进位和当前和的个位数
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+
+            //将l1，l2，结果向后移
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            cur = cur.next;
+        }
+        //若还有进位，记录
+        if(carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return res.next;
+    }
 }
