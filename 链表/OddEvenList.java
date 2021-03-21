4998a6b48ba6584ca8d826298e975262f97b31f6
Batpyt <Batpyt@users.noreply.github.com>
2021-03-21 09:50:08 +0800

    合并奇偶链表

--- 链表/OddEvenList.java
+++ 链表/OddEvenList.java
@@ -62,4 +62,20 @@ public class OddEvenList {
         odd.next = evenhead;
         return head;
     }
+
+    public ListNode oddEvenList3(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenhead = even;
+        while(even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenhead;
+        return head;
+    }
 }
