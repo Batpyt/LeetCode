fcb2049e37d5224f02cc81d2810190c6b871e2d8
tianyuping <tianyuping@fang.com>
2021-03-25 10:07:17 +0800

    删除排序链表中的重复元素

--- 链表/RemoveDuplicates.java
+++ 链表/RemoveDuplicates.java
@@ -40,4 +40,24 @@ public class RemoveDuplicates {
 		}
 		return dummy.next;
 	}
+
+	/**
+	 * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+	 */
+	public ListNode deleteDuplicates1(ListNode head) {
+		if(head == null) return head;
+		ListNode cur = head;
+		while(cur != null && cur.next != null) {
+			if(cur.val == cur.next.val) {
+				int temp = cur.val;
+				while(cur.next != null && cur.next.val == temp) {
+					cur.next = cur.next.next;
+				}
+			}
+			else {
+				cur = cur.next;
+			}
+		}
+		return head;
+	}
 }
